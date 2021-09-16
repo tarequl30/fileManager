3362509e73f4ad27217eece847d013dcc7da4612
apple <apple@apples-MacBook-Pro.local>
2021-09-16 11:42:28 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magni.</h1>
+        </div>
+  )
+}
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+  
+    <div>
+        <h1>weelcome to the web</h1>
+        </div>
+  )
+}
